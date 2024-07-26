28c902637c133b5162c840e077b6c6c22d6f52b2
Lucas Salvacio Husada <lucas.salvacio2005@gmail.com>
2024-07-26 16:18:25 +0700

    Continued Progress on CreateNewTransaction Form
    - added Number Format and NumPad Keyboard for amount field
    - Made Category Field and 2 Toggle Buttons

--- lib/transactions/createNewTransactionPage.dart
+++ lib/transactions/createNewTransactionPage.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CreateNewTransaction extends StatelessWidget{
   const CreateNewTransaction({super.key});
@@ -19,30 +20,40 @@ class _NewTransactionForm extends StatefulWidget{
   }
 }
 
+
 class _NewTransactionFormState extends State<_NewTransactionForm>{
 
   final _formKey = GlobalKey<FormState>();
   TextEditingController _dateController = TextEditingController();
-  DateTime? _picked = DateTime.now();
+  DateTime? _pickedDate = DateTime.now();
+  int _transactionTypeIdx = 0;
+  List<bool> _selectedButtons = List.generate(2, (_) => false);
+
 
   Future<void> _selectDate() async {
-    _picked = await showDatePicker(
+    _pickedDate = await showDatePicker(
       context: context,
-      initialDate: _picked,
+      initialDate: _pickedDate,
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
     );
 
-    if(_picked != null){
+    if(_pickedDate != null){
       setState(() {
-        _dateController.text = _picked.toString().split(" ")[0];
+        _dateController.text = _pickedDate.toString().split(" ")[0];
       });
     }else{
       setState(() {
-        _picked = DateTime.now();
-        _dateController.text = _picked.toString().split(" ")[0];
+        _pickedDate = DateTime.now();
+        _dateController.text = _pickedDate.toString().split(" ")[0];
       });
     }
+  }
+
+  void _selectTransactionType(int idx){
+    setState(() {
+      _transactionTypeIdx = idx;
+    });
   }
 
   @override
@@ -54,12 +65,51 @@ class _NewTransactionFormState extends State<_NewTransactionForm>{
           margin: const EdgeInsets.only(top: 50.0, left: 50.0, right: 50.0),
           child: Column(
             children: <Widget>[
+              // ListTile(
+              //   title: const Text('Income'),
+              //   leading: Radio<int>(
+              //     value: 0,
+              //     groupValue: _transactionTypeIdx,
+              //     onChanged: (int? value) {
+              //       setState(() {
+              //         _transactionTypeIdx = 0;
+              //       });
+              //     },
+              //   ),
+              // ),
+              // ListTile(
+              //   title: const Text('Expense'),
+              //   leading: Radio<int>(
+              //     value: 1,
+              //     groupValue: _transactionTypeIdx,
+              //     onChanged: (int? value) {
+              //       setState(() {
+              //         _transactionTypeIdx = 1;
+              //       });
+              //     },
+              //   ),
+              // ),
+              ToggleButtons(
+                isSelected: _selectedButtons,
+                onPressed: (int idx){
+                  setState(() {
+                    _selectedButtons.setAll(0, [_selectedButtons[idx], _selectedButtons[idx]]);
+                    _selectedButtons[idx] = !_selectedButtons[idx];
+                  });
+                },
+                children: const <Widget>[
+                  Text('Income'),
+                  Text('Expense'),
+                ],
+              ),
               TextFormField(
+
                 controller: _dateController,
                 decoration: const InputDecoration(
                   labelText: 'Date',
                   labelStyle: TextStyle(
-                      color: Colors.red
+                    color: Colors.red,
+                    fontSize: 15,
                   ),
                   filled: true,
                   prefixIcon: Icon(Icons.calendar_today),
@@ -69,7 +119,6 @@ class _NewTransactionFormState extends State<_NewTransactionForm>{
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.red),
                   ),
-
                 ),
                 readOnly: true,
                 onTap: () {
@@ -78,22 +127,35 @@ class _NewTransactionFormState extends State<_NewTransactionForm>{
               ),
               TextFormField(
                 decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Amount',
                     labelText: 'Amount',
                     labelStyle: TextStyle(
-                      color: Colors.red
+                      color: Colors.red,
+                      fontSize: 10,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.red
-                      ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    )
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    labelText: 'Category',
+                    labelStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 10,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
                     )
                 ),
               ),
             ],
-          ),
-        )
+          )
+        ),
       )
     );
   }
